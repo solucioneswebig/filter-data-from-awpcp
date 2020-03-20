04ca9b2ebc409b7b7c9b6d231e88f2b7e75e927a
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 15:36:17 -0400

    Update 20/03/2020 15:37

--- controllers/page.php
+++ controllers/page.php
@@ -14,10 +14,7 @@ global $wpdb;
             <input type="hidden" name="page" value="gestionar-datos-awpcp">
             <h3>Seleccionar categoria a filtrar</h3>
             <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <button type="submit" class="btn btn-success">Buscar resultados</button>
-            </div>
-            <select name="id_mostrar" class="form-control" id="">
+            <select name="id_mostrar" class="form-control" id="" aria-label="Example text with button addon" aria-describedby="button-addon1">
             <?php 
             $obtener_categorias = $wpdb->get_results("SELECT * FROM wp_awpcp_categories");
             foreach($obtener_categorias as $key => $value):
@@ -28,6 +25,9 @@ global $wpdb;
             <?php endforeach; ?>
 
             </select>
+            <div class="input-group-prepend">
+                <button type="submit" class="btn btn-success" id="button-addon1">Buscar resultados</button>
+            </div>
             </div>
         </form>        
         </div>
