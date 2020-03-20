71da15520c6f21c7d1e0c403a2d1bafc4e40be74
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 15:17:34 -0400

    Update 20/03/2020 15:17

--- controllers/listar.php
+++ controllers/listar.php
@@ -4,5 +4,33 @@ global $wpdb;
 
 $obtener_datos = $wpdb->get_results("SELECT * FROM wp_awpcp_ads where ad_category_id = ".$_GET['id_mostrar']."");
 
+?>
 
-var_dump($obtener_datos);
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titulo</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php 
+foreach($obtener_datos as $key => $value):
+?>
+    <tr>
+      <th scope="row"><?php echo $value->ad_id; ?></th>
+      <td><?php echo $value->ad_title; ?></td>
+      <td><?php echo $value->ad_contact_phone; ?></td>
+      <td><?php echo $value->ad_contact_name; ?></td>
+      <td><?php echo $value->ad_contact_email; ?></td>
+    </tr>
+<?php 
+endforeach;
+?> 
+  </tbody>
+</table>
