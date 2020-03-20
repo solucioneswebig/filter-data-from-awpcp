8bb5e677e0a54bbd88e50ab8e7226c2897472f88
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 14:30:15 -0400

    Update 20/03/2020 14:30

--- models/funciones.php
+++ models/funciones.php
@@ -5,3 +5,25 @@
 if ( ! defined( 'ABSPATH' ) ) exit; 
 
 
+/**
+ * Register a custom menu page.
+ */
+function wpdocs_register_my_custom_menu_page() {
+    add_menu_page(
+        __( 'GESTIONAR DATOS AWPCP', 'textdomain' ),
+        'custom menu',
+        'manage_options',
+        'cargar_page_dashboard',
+        '',
+        '',
+        6
+    );
+}
+add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+
+/**
+ * Display a custom menu page
+ */
+function cargar_page_dashboard(){
+    include GN_PLUGIN_DIR_PATH . "controllers/page.php";
+}
