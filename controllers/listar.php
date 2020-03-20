5df99f304b18ef0034f302b4690345fe8e30b542
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 15:34:00 -0400

    Update 20/03/2020 15:33

--- controllers/listar.php
+++ controllers/listar.php
@@ -7,30 +7,37 @@ $obtener_datos = $wpdb->get_results("SELECT * FROM wp_awpcp_ads where ad_categor
 ?>
 
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Titulo</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
 
-<?php 
-foreach($obtener_datos as $key => $value):
-?>
-    <tr>
-      <th scope="row"><?php echo $value->ad_id; ?></th>
-      <td><?php echo $value->ad_title; ?></td>
-      <td><?php echo $value->ad_contact_phone; ?></td>
-      <td><?php echo $value->ad_contact_name; ?></td>
-      <td><?php echo $value->ad_contact_email; ?></td>
-    </tr>
-<?php 
-endforeach;
-?> 
-  </tbody>
-</table>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titulo</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        <?php 
+        foreach($obtener_datos as $key => $value):
+        ?>
+            <tr>
+            <th scope="row"><?php echo $value->ad_id; ?></th>
+            <td><?php echo $value->ad_title; ?></td>
+            <td><?php echo $value->ad_contact_phone; ?></td>
+            <td><?php echo $value->ad_contact_name; ?></td>
+            <td><?php echo $value->ad_contact_email; ?></td>
+            </tr>
+        <?php 
+        endforeach;
+        ?> 
+        </tbody>
+        </table>
+     </div>
+   </div>
+</div>
--- controllers/page.php
+++ controllers/page.php
@@ -7,7 +7,7 @@ global $wpdb;
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-8">
         
         <form action="admin.php" style="margin-top: 20%;">
 
