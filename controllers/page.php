fd481a3c316cbb9748901622aa595222d420d2b2
solucioneswebig <alexanderr677@gmail.com>
2020-03-20 15:28:16 -0400

    Update 20/03/2020 15:25

--- controllers/page.php
+++ controllers/page.php
@@ -5,24 +5,32 @@ global $wpdb;
 
 ?>
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+        
+        <form action="admin.php">
 
-<form action="admin.php">
+            <input type="hidden" name="page" value="gestionar-datos-awpcp">
+            <div class="input-group">
+            <select name="id_mostrar" class="form-control" id="">
+            <?php 
+            $obtener_categorias = $wpdb->get_results("SELECT * FROM wp_awpcp_categories");
+            foreach($obtener_categorias as $key => $value):
+            ?>
 
-<input type="hidden" name="page" value="gestionar-datos-awpcp">
+            <option value="<?php echo $value->category_id; ?>"><?php echo $value->category_name; ?></option>
 
-<select name="id_mostrar" id="">
-<?php 
-$obtener_categorias = $wpdb->get_results("SELECT * FROM wp_awpcp_categories");
-foreach($obtener_categorias as $key => $value):
-?>
+            <?php endforeach; ?>
 
-<option value="<?php echo $value->category_id; ?>"><?php echo $value->category_name; ?></option>
-
-<?php endforeach; ?>
-
-</select>
+            </select>
 
 
-<button type="submit">Buscar resultados</button>
+            <button type="submit" class="btn btn-success">Buscar resultados</button>
+            </div>
+        </form>        
+        </div>
+    </div>
+</div>
 
-</form>
+
